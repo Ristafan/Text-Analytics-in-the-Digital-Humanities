668afba84f0cb41e65a82bfc5db58c22b1469399
MartinFaeh <121894103+Ristafan@users.noreply.github.com>
2025-04-28 14:12:49 +0200

    Feat: Stopword file creation

--- src/PreProcessing/NaturalLanguageProcessor.py
+++ src/PreProcessing/NaturalLanguageProcessor.py
@@ -1,5 +1,13 @@
 import regex as re
 from stop_words import get_stop_words
+from tqdm import tqdm
+
+
+def create_stop_words_file():
+    stop_words = get_stop_words('english')
+    with open('stopwords.txt', 'w', encoding='utf-8') as file:
+        for word in stop_words:
+            file.write(word + '\n')
 
 
 class NaturalLanguageProcessor:
@@ -17,10 +25,14 @@ class NaturalLanguageProcessor:
         self.text = re.sub(r'http\S+', '', self.text)
 
     def remove_stopwords(self):
-        stop_words = get_stop_words('english')
-        for word in self.text.split():
-            if word in stop_words:
-                self.text = re.sub(word, '', self.text)
+        stop_words = []
+        with open('PreProcessing/stopwords.txt', 'r', encoding='utf-8') as file:
+            for word in file.readlines():
+                stop_words.append(word.strip())
+
+        words = self.text.split()
+        filtered_words = [word for word in words if word not in stop_words]
+        self.text = ' '.join(filtered_words)
 
 
 if __name__ == "__main__":
