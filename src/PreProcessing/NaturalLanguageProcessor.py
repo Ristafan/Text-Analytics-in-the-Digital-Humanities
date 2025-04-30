857a9b5faa2eadeafdb45aca82f78d913f67cd6d
MartinFaeh <121894103+Ristafan@users.noreply.github.com>
2025-04-30 17:54:15 +0200

    Feat: Improved Text cleaning and added stopword

--- src/PreProcessing/NaturalLanguageProcessor.py
+++ src/PreProcessing/NaturalLanguageProcessor.py
@@ -11,8 +11,15 @@ def create_stop_words_file():
 
 
 class NaturalLanguageProcessor:
-    def __init__(self, text):
+    def __init__(self, text, lower=True, remove_links=True, remove_stopwords=True, remove_reddit_formatting=True, remove_stars=True, remove_punctuations=True, remove_emojis=True):
         self.text = text
+        self.methods_bool = [lower, remove_links, remove_stopwords, remove_reddit_formatting, remove_stars, remove_punctuations, remove_emojis]
+        self.methods = [self.lower, self.remove_links, self.remove_stopwords, self.remove_reddit_formatting, self.remove_stars, self.remove_punctuations, self.remove_emojis]
+
+        for i in range(len(self.methods)):
+            if self.methods_bool[i]:
+                print(self.text)
+                self.methods[i]()
 
     def save_txt(self, file_path):
         with open(file_path, 'w', encoding='utf-8') as file:
@@ -26,7 +33,8 @@ class NaturalLanguageProcessor:
 
     def remove_stopwords(self):
         stop_words = []
-        with open('PreProcessing/stopwords.txt', 'r', encoding='utf-8') as file:
+        # PreProcessing/stopwords.txt
+        with open('stopwords.txt', 'r', encoding='utf-8') as file:
             for word in file.readlines():
                 stop_words.append(word.strip())
 
@@ -34,12 +42,43 @@ class NaturalLanguageProcessor:
         filtered_words = [word for word in words if word not in stop_words]
         self.text = ' '.join(filtered_words)
 
+    def remove_reddit_formatting(self):
+        self.text = re.sub(r'\*', '', self.text)  # bold text
+
+    def remove_stars(self):  # often stars are used for bold formatting
+        self.text = self.text.replace('*', '')
+
+    def remove_punctuations(self):
+        self.text = re.sub(r'[^\w\s]', '', self.text)
+
+    def remove_emojis(self):
+        emoj = re.compile("["
+                          u"\U0001F600-\U0001F64F"  # emoticons
+                          u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                          u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                          u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                          u"\U00002500-\U00002BEF"  # chinese char
+                          u"\U00002702-\U000027B0"
+                          u"\U000024C2-\U0001F251"
+                          u"\U0001f926-\U0001f937"
+                          u"\U00010000-\U0010ffff"
+                          u"\u2640-\u2642"
+                          u"\u2600-\u2B55"
+                          u"\u200d"
+                          u"\u23cf"
+                          u"\u23e9"
+                          u"\u231a"
+                          u"\ufe0f"  # dingbats
+                          u"\u3030"
+                          "]+", re.UNICODE)
+        self.text = re.sub(emoj, '', str(self.text))
+        self.text = ' '.join(self.text.split())
+
 
 if __name__ == "__main__":
     # Example usage
-    text = "This is a sample text with a link http://example.com and some stop words."
+    text = "This is a **sample** text* * *with ;a ] link http://example.com and some \" $stop words ✍ 😉🌷 📌 👈🏻 🖥."
     nlp = NaturalLanguageProcessor(text)
-    nlp.lower()
-    nlp.remove_links()
-    nlp.remove_stopwords()
+    print()
+    print(text)
     print(nlp.text)
